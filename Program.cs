c6b09070c626ecdc3efd55ca0e9b9372422070e5
GabrielTAlberton <gtalberton@gmail.com>
2024-05-27 19:50:19 -0300

    some changes

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@
 
 using classesMetodosPropriedaeds.models;
 
+
+// Classe carro teste com get set e utilizacao das propriedades em um metodo
 Carro novoCarro = new Carro();
 novoCarro.Montadora = "GM";
 novoCarro.Modelo = "Corsa";
 novoCarro.Ano = 2010;
 novoCarro.Valor = 22500;
-
 novoCarro.ApresentarCarro();
